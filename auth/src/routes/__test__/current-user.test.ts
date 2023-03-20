4c9cf6a133070049e658f265b5a52bced8490145
Avijit Sarkar <avijit.a.sarkar@capgemini.com>
2023-03-20 21:54:51 +0530

    fixed test case

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -8,7 +8,7 @@ it('should return the details of current logged in user', async () => {
     .get('/api/users/current-user')
     .set('Cookie', cookie)
     .send()
-    .expect(400);
+    .expect(200);
   expect(response.body.currentUser.email).toEqual('test@test.com');
 });
 
